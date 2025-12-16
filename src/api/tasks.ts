7aba8b0c608ca7d3d84af7c3632e79b63c2791c0
Sabyrka <sabyrka18@outlook.com>
2025-12-17 02:07:18 +0500

    feat: list detail (#4)

--- src/api/tasks.ts
+++ src/api/tasks.ts
@@ -8,3 +8,15 @@ export const getTasks = async (): Promise<Task[]> => {
   const json: { data: Task[] } = await res.json()
   return json.data
 }
+
+export const getTask = async (boardId: string, taskId: string): Promise<Task> => {
+  const res = await fetch(
+    `https://trelly.it-incubator.app/api/1.0/boards/${boardId}/tasks/${taskId}`,
+    {
+      headers: { 'api-key': API_KEY },
+    },
+  )
+
+  const json: { data: Task } = await res.json()
+  return json.data
+}
--- src/types/task.ts
+++ src/types/task.ts
@@ -1,8 +1,11 @@
 export type TaskAttributes = {
-  status: number
+  status: 0 | 1 | 2
+  priority: 1 | 2 | 3 | 4 | 5
   addedAt: string
-  priority: number
   title: string
+  boardId: string
+  boardTitle: string
+  description: string | null
 }
 
 export type Task = {
